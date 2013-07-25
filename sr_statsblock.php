e1b9da3cc241c279915e877517b81c2d75e920e0
kibje <edeklerk@gmail.com>
2013-07-25 16:39:17 +0200

    close mysql connections properly

--- sr_statsblock.php
+++ sr_statsblock.php
@@ -12,15 +12,15 @@ $res->close();
 $res=$con->query("SELECT MIN(firstseen) FROM userstats");
 $row=$res->fetch_row();
 $userssince=gmdate("Y-m-d", $row[0]);
+$res->close();
 
-$thisweek = get_this_week();
-$alltime = get_all_times();
+$con->close();
 
 echo 
 "<table border='1' align='center' style='font-size:13px'>
 <tr><td style='width:400px; font-weight:bold;'>Addons</td><td style='text-align:center'>" . $amount ."</td></tr>
 <tr><td style='width:400px; font-weight:bold;'>Unique users since ".$userssince."</td><td style='text-align:center'>" . $users ."</td></tr>
-<tr><td style=' font-weight:bold;'>Downloads last 7 days</td><td style='text-align:center'> ".$thisweek." </td></tr>
-<tr><td style=' font-weight:bold;'>Downloads since 2013-07-22</td><td style='text-align:center'> ".$alltime." </td></tr>
+<tr><td style=' font-weight:bold;'>Downloads last 7 days</td><td style='text-align:center'> ".get_this_week()." </td></tr>
+<tr><td style=' font-weight:bold;'>Downloads since 2013-07-22</td><td style='text-align:center'> ".get_all_times()." </td></tr>
 </table>";
 ?>
