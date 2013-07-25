dfc4372372e2d66db404244044390bf08c7de130
kibje <edeklerk@gmail.com>
2013-07-25 16:11:04 +0200

    typo

--- sr_addonstat.php
+++ sr_addonstat.php
@@ -6,7 +6,7 @@ if ($addon==""){
 }else{
     include("connect.php");
     $con=new mysqli($server,$user,$password,$database);
-    if ($res = $con->query("SELECT SUM(stats) FROM download WHERE addonid='$addon'")){
+    if ($res = $con->query("SELECT SUM(stats) FROM download WHERE addonid='".$addon."'")){
         $row = $res->fetch_row();
         printf(number_format((float)$row[0],0,",","."));
         }
