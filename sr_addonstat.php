678f454d215194496aa81d7d7034c6813b1f5d83
kibje <edeklerk@gmail.com>
2013-07-26 14:38:35 +0200

    use functions.php with addonstat

--- sr_addonstat.php
+++ sr_addonstat.php
@@ -1,19 +1,19 @@
 <?php
+include("functions.php");
 $addon="";
 if (isset($_GET['addon']) && !empty($_GET['addon'])){
     $con= db_connect();
     $addon = $con->real_escape_string($_GET['addon']);
     $con->close();
 }
-
-//$addon="skin.neon";
 if ($addon==""){
-    echo "error"; 
-}else{
-    include("connect.php");
+    echo "no addon specified"; 
+}
+else{
     $con=new mysqli($server,$user,$password,$database);
     if ($res = $con->query("SELECT SUM(stats) FROM download WHERE addonid='".$addon."'")){
         $row = $res->fetch_row();
+	$res->close();
         printf(number_format((float)$row[0],0,",","."));
         }
     $con->close();
